13ae2e9ec00cfbc43b02a44df6afb692557c0fee
hervekwizera <hervekwizera63@gmail.com>
2025-02-17 01:12:25 +0200

    recursive function to make countdown very slowly by using setTimeout()

--- countDown.js
+++ countDown.js
@@ -8,4 +8,18 @@ function countDown(num){
    return countDown(num-1)
    
 }
-console.log(countDown(10));
+//console.log(countDown(10));
+//using setTimeout() tod delay countdown
+
+function countdown(n) {
+   if (n < 0) {
+       console.log("Done!");
+       return;
+   }
+   console.log(n);
+   setTimeout(() => countdown(n - 1), 1000); // Wait 1 second before next call
+}
+
+// Example usage
+countdown(10);
+
