5e459625e672b30b6566ecc3445255f17bb561b2
hervekwizera <hervekwizera63@gmail.com>
2025-03-13 14:19:45 +0200

    a function that finds missing letters in a sequence.

---
+++ findMissingLetter.js
@@ -0,0 +1,10 @@
+function findMissingLetter(str) {
+    for (let i = 0; i < str.length - 1; i++) {
+        if (str.charCodeAt(i + 1) - str.charCodeAt(i) > 1) {
+            return String.fromCharCode(str.charCodeAt(i) + 1);
+        }
+    }
+    return null;
+}
+
+console.log(findMissingLetter("abce")); // "d"
