853eebd7b07d8c9131347a35880344416d990c2e
hervekwizera <hervekwizera63@gmail.com>
2025-04-29 21:39:40 +0200

    .

--- Oops/capitalizeFirstLetter.js
+++ Oops/capitalizeFirstLetter.js
@@ -6,3 +6,12 @@ function capitalizeWords(sentence) {
 }
 
 console.log(capitalizeWords("hello world!")); // "Hello World!"
+//################################
+// alternative way
+function capitalizeWords(sentence) {
+    return sentence
+        .split(' ')
+        .reduce((acc, word) => acc + (acc ? ' ' : '') + word.charAt(0).toUpperCase() + word.slice(1), '');
+}
+
+console.log(capitalizeWords("hello world!")); // "Hello World!"
